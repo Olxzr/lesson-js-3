e13bd7ea8760d09e3d98a9a1d033819a7988a318
olxzr <olxazar96@mail.ru>
2024-02-27 12:48:30 +0300

    сделал 6 заданий

---
+++ index.js
@@ -0,0 +1,63 @@
+
+// Создание объектов с помощью литерала объекта 
+// 1 Создайте объект person с полями name, age и job. Присвойте полям значения, например, "John", 25 и "developer" соответственно.
+
+// let person = {
+//     name: 'Jhon',
+//     age: 25,
+//     job: 'developer',
+// }
+
+// Изменение свойств объекта
+
+// person.name = 'Alex'
+// person["job"] = "Maller"
+
+// console.log(person)
+
+// 2 Создайте объект car с полями brand, model, year и color. Присвойте полям значения, например, "Toyota", "Camry", 2020 и "blue" соответственно.
+
+// let car = {
+//     brand: 'Toyota',
+//     model: 'Camry',
+//     year: 2020,
+//     color: 'blue'
+// }
+
+// // Добавление новых свойств в объект
+
+// car.fuel = 'petrol';
+// car.drive = '4x4'
+
+// console.log(car)
+
+
+// Создание объекта с использованием конструктора объекта
+// 1 Создайте функцию-конструктор Person с параметрами name, age и job. Создайте новый объект с использованием этого конструктора и присвойте значения для полей.
+
+// function person(name, age, job){
+//     this.name = name;
+//     this.age = age;
+//     this.job = job;
+// }
+// let person1 = new person('Vladik', 25, 'reseller')
+
+// person1.name = 'Alex'
+// person1["job"] = "Maller"
+
+// console.log(person1)
+
+
+
+// 2 Создайте функцию-конструктор Car с параметрами brand, model, year и color. Создайте новый объект с использованием этого конструктора и присвойте значения для полей.
+
+// function Car(brand, model, year, color){
+//     this.brand = brand;
+//     this.model = model;
+//     this.year = year;
+//     this.color = color;
+// }
+// let car1 = new Car('Lamborghini', 'Urus', 2018, 'black')
+
+// console.log(car1)
+
